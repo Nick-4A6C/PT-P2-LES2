ea32ca29c9e73fbda931998b49bfa226199128f7
Nick-4A6C <516984@edu.rocmn.nl>
2016-11-25 15:32:50 +0100

    totaalbedrag.html

--- totaalbedrag.js
+++ totaalbedrag.js
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Sjabloon</title>
-    </head>
-    <body>
-        <script>
-        
-        </script>
-    </body>
-</html>
+function berekenAantal(){
+    var bedrag = document.getElementById("bedrag").value;
+    var aantal = document.getElementById("aantal").value;
+    var totaalBedrag = aantal * bedrag;
+    document.getElementById("totaalBedrag").innerHTML = "Het totale bedrag is: " + totaalBedrag.toFixed(2);
+}
